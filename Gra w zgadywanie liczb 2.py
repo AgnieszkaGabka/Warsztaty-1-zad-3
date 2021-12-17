5b19db8c693122901a3960fbfd2e5abc29c6e6ac
AgnieszkaGabka <94054240+AgnieszkaGabka@users.noreply.github.com>
2021-12-17 19:00:18 +0100

    Add files via upload
    Code changed to english according to instruction in task description

--- Gra w zgadywanie liczb 2.py
+++ Gra w zgadywanie liczb 2.py
@@ -1,29 +1,29 @@
-def zgadnij():
-    print("Pomyśl liczbę od 0 do 1000 a ja ją zgadnę w max. 10 próbach")
+def shot():
+    print("Think of a number from 0 to 1000 and I will guess it in max. 10 tries")
 
-    dobrze = False
+    good = False
     min = 0
     max = 1000
-    while not dobrze: #dopóki komputer nie zgdanie liczby gracza, wykonaj poniższe działania
+    while not good: #dopóki komputer nie zgdanie liczby gracza, wykonaj poniższe działania
         guess = int((max - min) / 2) + min #oblicznie zgadywanej liczby
-        print(f"Zgaduję: {guess}")
+        print(f"My guess: {guess}")
         try: #wprowadzenie odpowiedzi przez gracza
-            a = (int(input("Wybierz odpowiedź: \
-            1 - za dużo\
-            2 - za mało\
-            3 - zgadłeś")))
+            a = (int(input("Choose the answer: \
+            1 - too big\
+            2 - too small\
+            3 - you guessed")))
             if a == 3:
-                print("Wygrałem!")
-                dobrze = True
+                print("I win!")
+                good = True
             elif a == 2:
                 min = guess #jeśli za mało, przelicz wartość zgadywanej liczby bazując na poprzednio zgadywanej liczbie
             elif a == 1:
                 max = guess #jeśli za dużo, przelicz wartość zgadywanej liczby bazując na poprzednio zgadywanej liczbie
             else:
-                print("Nie oszukuj!")
+                print("Do not cheatj!")
         except (ValueError, TypeError):
-            print("Wprowadż liczbę!") #jeśli wrowadzona dana nie jest liczbą lub nie mieści się w przedziale 1-1000, wyświetl odpowiedź
+            print("Enter a correct number!") #jeśli wrowadzona dana nie jest liczbą lub nie mieści się w przedziale 1-1000, wyświetl odpowiedź
 
 
-print(zgadnij())
+print(shot())
 
